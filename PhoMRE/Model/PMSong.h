d899b84806b6278f86d092f18b7c2540b87ce324
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-06-07 15:47:37 -0500

    initial commit

---
+++ PhoMRE/Model/PMSong.h
@@ -0,0 +1,13 @@
+//
+//  PMSong.h
+//  PhoMRE
+//
+//  Created by Joseph Malandruccolo on 6/7/13.
+//  Copyright (c) 2013 Joseph Malandruccolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PMSong : NSObject
+
+@end
---
+++ PhoMRE/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PhoMRE
+//
+//  Created by Joseph Malandruccolo on 6/7/13.
+//  Copyright (c) 2013 Joseph Malandruccolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
